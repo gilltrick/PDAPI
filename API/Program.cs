a07f7d0a0293c4494ec85659a9c2dcfee2fb9519
Gilltrick <patrickgillmann@web.de>
2023-04-03 22:01:54 +0200

    initial commit

---
+++ API/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class API{
+
+    private static HttpClient client = new HttpClient();
+    public static  void Main(){
+
+        InitClient();
+        Post(GetTestData());
+    
+        string input = Console.ReadLine();
+        
+    }
+
+    static void InitClient(){
+
+        client.BaseAddress = new Uri("http://192.168.2.100:1234");
+    }
+
+    static string GetTestData(){
+
+        return "{\"user\":\"user\",\"pass\":\"1234\"}";
+    }
+
+    public static async void Post(string json){
+
+        StringContent content = new StringContent(json, Encoding.UTF8, "application/json"); 
+        HttpResponseMessage result = await client.PostAsync("push", content);
+        string resultContent = await result.Content.ReadAsStringAsync();   
+        Console.WriteLine(resultContent);
+    } 
+}
---
+++ Server/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import os, json
+
+server = Flask(__name__)
+
+@server.route("/push", methods=["post"])
+def push():
+    data = request.json
+    # SaveData(data)
+    print(data)
+    return data
+
+def SaveData(data):
+    file = open(os.getcwd()+"/file.json", "w")
+    json.dump(data, file)
+
+if __name__ == "__main__":
+    server.run(debug=True, host="0.0.0.0", port=1234)
